24a838f0ed409e0fddb8bcf042fd81b62450d75b
ahmedhosam-dev <ahmed.hossam.t.11@gmail.com>
2024-05-25 18:24:34 +0300

    Add log endpoint

--- index.js
+++ index.js
@@ -54,6 +54,65 @@ app.post("/api/users", (req, res) => {
   });
 });
 
+// Exercises
+app.post("/api/users/:_id/exercises", async (req, res) => {
+  const userName = await User.findById(req.params._id);
+  let userDate = req.body.date;
+
+  if (req.body.date) {
+    let date = req.body.date.split("-");
+    userDate = new Date(date[0], date[1] - 1, date[2]).toDateString();
+  }
+
+  const newExercies = new Exercise({
+    _id: req.params._id,
+    username: userName.username,
+    description: req.body.description,
+    duration: req.body.duration,
+    date: userDate,
+  });
+
+  // newExercies.save();
+
+  if ((await Log.findById(req.params._id)) === null) {
+    const newLog = new Log({
+      _id: req.params._id,
+      username: userName.username,
+      count: 1,
+      log: [
+        {
+          description: req.body.description,
+          duration: req.body.duration,
+          date: userDate,
+        },
+      ],
+    });
+
+    newLog.save();
+  }
+
+  const log = await Log.findById(req.params._id);
+  log.count += 1;
+  log.log.push({
+    description: req.body.description,
+    duration: req.body.duration,
+    date: userDate,
+  });
+
+  log.save();
+
+  res.json(newExercies);
+});
+
+// Log
+app.get("/api/users/:_id/logs", async (req, res) => {
+  if ((await Log.findById(req.params._id)) === null)
+    return res.status(500).json({ error_message: "User not found!" });
+
+  const log = await Log.findById(req.params._id);
+  res.status(200).json(log);
+});
+
 // Listener
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
